934cab63dc7ac2234900d7becaaa3c35628794a8
GregoryPardini <47989715+GregoryPardini@users.noreply.github.com>
2024-08-26 03:03:22 +0200

    initial commit

---
+++ example/gazelle_hosting_plugin_example.dart
@@ -0,0 +1,6 @@
+import 'package:gazelle_hosting_plugin/gazelle_hosting_plugin.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/gazelle_hosting_plugin_test.dart
@@ -0,0 +1,16 @@
+import 'package:gazelle_hosting_plugin/gazelle_hosting_plugin.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
